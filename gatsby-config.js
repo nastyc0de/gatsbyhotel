c1d36b70294cf27ff92564f2344efddaaf976667
nasty <plague@outlook.es>
2020-10-06 22:23:09 -0400

    4to commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,11 @@ module.exports = {
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options:{
+        apiToken: `e00f42869f4bb70510c9e6b3d8277a`,
+      }
+    }
   ],
 }
---
+++ sas
@@ -0,0 +1,1 @@
+uANHiPYr25C4rtN
